9f278ff074e6ddebc8f2db492756667870136ade
renan tomazini <renantomazini@gmail.com>
2021-09-23 22:03:22 -0300

    Adicionando o primeiro if e um esboço do que faremos

--- mips.py
+++ mips.py
@@ -1,4 +1,6 @@
 entrada = open('entrada.txt', 'r') #abre o arquivo
 
 for instrucao in entrada: #para cada linha na entrada
-    print(instrucao)
+    instrucao = instrucao[2:] #retira o 0x
+    if instrucao[:2] == "00":
+        print("é aritmetico")
