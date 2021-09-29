cb72b1de83d347e89d7b38fddb460a59479ba03e
renan tomazini <renantomazini@gmail.com>
2021-09-29 12:42:42 -0300

    Convertendo as entradas em binario

--- mips.py
+++ mips.py
@@ -2,5 +2,6 @@ entrada = open('entrada.txt', 'r') #abre o arquivo
 
 for instrucao in entrada: #para cada linha na entrada
     instrucao = instrucao[2:] #retira o 0x
-    if instrucao[:2] == "00":
-        print("é aritmetico")
+    instrucao = bin(int(instrucao, 32))
+    print(instrucao)
+        
