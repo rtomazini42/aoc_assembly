74f79b5b39d0efa8cf8ac0cc8a3f03db5598d933
renan tomazini <renantomazini@gmail.com>
2021-09-30 20:32:46 -0300

    0

--- mips.py
+++ mips.py
@@ -4,6 +4,7 @@ for instrucao in entrada: #para cada linha na entrada
     instrucao = instrucao[2:] #retira o 0x
     instrucao = bin(int(instrucao, 32))
     print(instrucao)
-
+    #para cada op code nós temos que fazer um switch case
+    #o mesmo para os seguintes
 
 entrada.close()
