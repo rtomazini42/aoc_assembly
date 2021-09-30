0e7faa40fa0695bf62698e762bc32aff7d77d37d
renan tomazini <renantomazini@gmail.com>
2021-09-30 20:30:27 -0300

    Comentarios e fechamento de arquivo

--- mips.py
+++ mips.py
@@ -4,4 +4,6 @@ for instrucao in entrada: #para cada linha na entrada
     instrucao = instrucao[2:] #retira o 0x
     instrucao = bin(int(instrucao, 32))
     print(instrucao)
-        
+
+
+entrada.close()
